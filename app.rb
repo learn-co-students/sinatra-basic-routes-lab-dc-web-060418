ccc59f61de4ebd75b9529cd8db0c746636154f09
github username <github email address>
2018-06-24 15:14:59 -0400

    Wrote app class with routes

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get("/name") {"My name is August"}
+
+  get ("/hometown") {"My hometown is Silver Spring, MD"}
+
+  get ("/favorite-song") {"My favorite song is Case of You, by Joni Mitchell"}
+
 end
