c4844e51948d48398b4b9f81a9970d3597d0abcb
Essa Hijazi <github email address>
2018-06-26 08:53:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Essa Hijazi"
+  end
+
+  get "/hometown" do
+    "My hometown is Damascus, Syria"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is New York, New York"
+  end
+
 end
